975b8aae61efa62887301728acd00f3144dc4269
nishant1120 <nishant.mishra1120@gmail.com>
2021-05-07 20:22:47 +0530

    Fixed Ui Bugs

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,18 +3,18 @@ import { Col } from "react-bootstrap";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import ProgressBar from "./../Progressbar.js";
-// import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-// import ChangingProgressProvider from "./ChangingProgressProvider";
+import ChangingProgressProvider from "./ChangingProgressProvider";
 
 const Techstack = (prop) => {
   return (
     <>
-      {/* <div class="flip-card"> */}
-        {/* <div class="flip-card-inner"> */}
-          {/* <div class="flip-card-back">
-            <div>{prop.iconType}</div> */}
-            {/* <div label="Custom animation speed">
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-back">
+            <div>{prop.iconType}</div> 
+            <div label="Custom animation speed">
               <ChangingProgressProvider values={[0, prop.iconPercent]}>
                 {(percent) => (
                   <CircularProgressbar
@@ -30,13 +30,13 @@ const Techstack = (prop) => {
                   />
                 )}
               </ChangingProgressProvider>
-            </div> */}
-          {/* </div> */}
+            </div> 
+          </div>
           <Col xs={4} md={2} className="tech-icons">
             <i className={`${prop.iconName} tech-icon-images`}></i>
           </Col>
-        {/* </div> */}
-      {/* </div> */}
+        </div>
+       </div>
     </>
   );
 };
